03625bc584ff0a556126abec59c0058b8c239525
lkorzechowski <lkorzechowski@gmail.com>
2021-05-13 18:13:02 +0200

    0.004 dziala dzwonienie w glownej aktywnosci

--- app/src/main/java/com/orzechowski/prototyp/mainrecycler/ListAdapter.java
+++ app/src/main/java/com/orzechowski/prototyp/mainrecycler/ListAdapter.java
@@ -1,27 +1,36 @@
 package com.orzechowski.prototyp.mainrecycler;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.orzechowski.prototyp.R;
 import com.orzechowski.prototyp.objects.NumerAlarmowy;
-
 import java.util.List;
 
 
-public class ListAdapter extends RecyclerView.Adapter<ListAdapter.NumeryViewHolder> {
-    private List<NumerAlarmowy> numery;
-    private final LayoutInflater mPompka;
 
-    public ListAdapter(Activity kontekst, List<NumerAlarmowy> listaNumerow) {
+public class ListAdapter extends RecyclerView.Adapter<ListAdapter.NumeryViewHolder> {
+    private final List<NumerAlarmowy> numery;
+    private final LayoutInflater mPompka;
+    private final Context context;
+    private final int REQUEST_PHONE_CALL = 1;
+
+    public ListAdapter(Activity kontekst, List<NumerAlarmowy> listaNumerow, Context appApcontext) {
         mPompka = kontekst.getLayoutInflater();
         this.numery = listaNumerow;
+        this.context = appApcontext;
     }
 
     @NonNull
@@ -37,6 +46,17 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.NumeryViewHold
         TextView usluga = numeryholder.itemView.findViewById(R.id.usluga);
         numer.setText(String.valueOf(numery.get(numerWiersza).getNumerTelefonu()));
         usluga.setText(numery.get(numerWiersza).getNazwaUslugi());
+
+        Button przyciskDzwon = numeryholder.itemView.findViewById(R.id.przycisk_zadzwon);
+        przyciskDzwon.setOnClickListener(v -> {
+            Intent phone_intent = new Intent(Intent.ACTION_CALL);
+            phone_intent.setData(Uri.parse("tel:" + 123));
+            if (ContextCompat.checkSelfPermission(context, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions((Activity)context, new String[]{Manifest.permission.CALL_PHONE},REQUEST_PHONE_CALL);
+            } else {
+                context.startActivity(phone_intent);
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/orzechowski/prototyp/mainrecycler/Recycler.java
+++ app/src/main/java/com/orzechowski/prototyp/mainrecycler/Recycler.java
@@ -3,15 +3,13 @@ package com.orzechowski.prototyp.mainrecycler;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.orzechowski.prototyp.R;
 import com.orzechowski.prototyp.objects.NumerAlarmowy;
-
+import org.jetbrains.annotations.NotNull;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -25,7 +23,7 @@ public class Recycler extends Fragment {
 
     @Override
     public View onCreateView(
-            LayoutInflater inflater, ViewGroup container,
+            @NotNull LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
         ) {
             List<NumerAlarmowy> numery = new LinkedList<>();
@@ -34,7 +32,7 @@ public class Recycler extends Fragment {
             for(int i = 0; i < uslugi.length; i++){
                 numery.add(new NumerAlarmowy(telefony[i], uslugi[i]));
             }
-            adapter = new ListAdapter(getActivity(), numery);
+            adapter = new ListAdapter(requireActivity(), numery, getContext());
             View view = inflater.inflate(R.layout.fragment_recycler, container, false);
             recycler = view.findViewById(R.id.numery_rv);
             recycler.setAdapter(adapter);
