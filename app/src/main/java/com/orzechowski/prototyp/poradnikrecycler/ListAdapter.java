7b18ff0dd7c8747c460cd7c59f5340bc390716d8
lkorzechowski <lkorzechowski@gmail.com>
2021-05-19 15:15:08 +0200

    0.015 almost the sort of implementation I want for autoplay, needs debugging

--- app/src/main/java/com/orzechowski/prototyp/poradnikrecycler/ListAdapter.java
+++ app/src/main/java/com/orzechowski/prototyp/poradnikrecycler/ListAdapter.java
@@ -16,25 +16,23 @@ import java.util.List;
 public class ListAdapter extends RecyclerView.Adapter<ListAdapter.InstrukcjeViewHolder> {
     private final List<Instrukcja> mInstructions;
     private final LayoutInflater mInflater;
-    private final Activity mActivity;
-    private final List<InstrukcjeViewHolder> mViewHolders;
-    private Player mCurrentPlayer;
-    private ClickedTitleListener mClickedTitleListener;
+    private final List<InstrukcjeViewHolder> mInstructionSetList;
+    private OnViewClickListener onViewClickListener;
 
     public ListAdapter(Activity tutorialActivity, List<Instrukcja> instructionsList,
-                       ClickedTitleListener clickedTitleListener) {
+                       OnViewClickListener listenerFromSuperclass)
+    {
         this.mInflater = tutorialActivity.getLayoutInflater();
         this.mInstructions = instructionsList;
-        this.mActivity = tutorialActivity;
-        this.mViewHolders = new LinkedList<>();
-        this.mClickedTitleListener = clickedTitleListener;
+        this.mInstructionSetList = new LinkedList<>();
+        this.onViewClickListener = listenerFromSuperclass;
     }
 
     @NonNull
     @Override
     public InstrukcjeViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
         View row = mInflater.inflate(R.layout.wiersz_instrukcje, null);
-        return new InstrukcjeViewHolder(row, mClickedTitleListener);
+        return new InstrukcjeViewHolder(row, onViewClickListener);
     }
 
     @Override
@@ -45,6 +43,7 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.InstrukcjeView
         instructionsView.setVisibility(View.GONE);
         title.setText(mInstructions.get(rowNumber).getTitle());
         instructionsView.setText(mInstructions.get(rowNumber).getInstructionSet());
+        mInstructionSetList.add(rowNumber, instrukcjeholder);
     }
 
     @Override
@@ -52,83 +51,42 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.InstrukcjeView
         return mInstructions.size();
     }
 
-    @Override
-    public void onViewAttachedToWindow(@NonNull InstrukcjeViewHolder holder) {
-        if(holder.getAdapterPosition()==1) play(0);
-    }
-
     public class InstrukcjeViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         TextView title, instruct;
         View divider;
-        ClickedTitleListener holderListener;
+        OnViewClickListener listenerForThisRow;
 
-        public InstrukcjeViewHolder(@NonNull View viewForRow, ClickedTitleListener listener){
+        public InstrukcjeViewHolder(@NonNull View viewForRow, OnViewClickListener listenerFromSuperClass){
             super(viewForRow);
             this.title = viewForRow.findViewById(R.id.tytul);
             this.instruct = viewForRow.findViewById(R.id.instrukcja);
-            this.divider= viewForRow.findViewById(R.id.rv_divider);
-            this.holderListener = listener;
-            this.title.setOnClickListener(this);
-            mViewHolders.add(this);
+            this.divider = viewForRow.findViewById(R.id.rv_divider);
+            this.listenerForThisRow = listenerFromSuperClass;
+            mInstructionSetList.add(this);
+            viewForRow.setOnClickListener(this);
         }
 
         @Override
-        public void onClick(View v) {
-            play(getAdapterPosition());
-            holderListener.onTitleClicked(getAdapterPosition());
+        public void onClick(View v){
+            listenerForThisRow.onViewClick(getAdapterPosition());
         }
     }
 
-    public interface ClickedTitleListener{
-        void onTitleClicked(int position);
+    public void expandAt(int position){
+        InstrukcjeViewHolder currentView = mInstructionSetList.get(position);
+        currentView.title.setVisibility(View.GONE);
+        currentView.instruct.setVisibility(View.VISIBLE);
+        currentView.divider.setVisibility(View.GONE);
     }
 
-    public class Player extends Thread {
-        private final int time, line;
-        private final TextView title, instruction;
-        private final View divider;
-
-        Player(int position){
-            line = position;
-            time = mInstructions.get(line).getTime();
-            InstrukcjeViewHolder currentView = mViewHolders.get(line);
-            title = currentView.title;
-            instruction = currentView.instruct;
-            divider = currentView.divider;
-        }
-
-        @Override
-        public void run(){
-            mActivity.runOnUiThread(() -> {
-                title.setVisibility(View.GONE);
-                instruction.setVisibility(View.VISIBLE);
-                divider.setVisibility(View.GONE);
-            });
-            try {
-                Thread.sleep(time);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-                return;
-            }
-            mActivity.runOnUiThread(() -> {
-                title.setVisibility(View.VISIBLE);
-                instruction.setVisibility(View.GONE);
-                divider.setVisibility(View.VISIBLE);
-                play(line+1);
-            });
-        }
+    public void hideAt(int position){
+        InstrukcjeViewHolder currentView = mInstructionSetList.get(position);
+        currentView.title.setVisibility(View.VISIBLE);
+        currentView.instruct.setVisibility(View.GONE);
+        currentView.divider.setVisibility(View.VISIBLE);
     }
 
-    public void play(int position){
-        if(mCurrentPlayer!=null){
-            mCurrentPlayer.title.setVisibility(View.VISIBLE);
-            mCurrentPlayer.instruction.setVisibility(View.GONE);
-            mCurrentPlayer.divider.setVisibility(View.VISIBLE);
-            mCurrentPlayer.interrupt();
-        }
-        if(position < mViewHolders.size()) {
-            mCurrentPlayer = new Player(position);
-            mCurrentPlayer.start();
-        }
+    public interface OnViewClickListener{
+        void onViewClick(int position);
     }
 }
--- app/src/main/java/com/orzechowski/prototyp/poradnikrecycler/Recycler.java
+++ app/src/main/java/com/orzechowski/prototyp/poradnikrecycler/Recycler.java
@@ -1,7 +1,6 @@
 package com.orzechowski.prototyp.poradnikrecycler;
 
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
@@ -16,13 +15,17 @@ import org.jetbrains.annotations.NotNull;
 import java.util.LinkedList;
 import java.util.List;
 
-public class Recycler extends Fragment implements ListAdapter.ClickedTitleListener {
-    protected RecyclerView recycler;
-    protected ListAdapter adapter;
-    List<Instrukcja> mInstructionsList = new LinkedList<>();
+public class Recycler extends Fragment implements ListAdapter.OnViewClickListener {
+    protected RecyclerView mRecycler;
+    protected ListAdapter mAdapter;
+    private final List<Instrukcja> mInstructionsList;
+    private Scroller mScrollerInstance;
+    private boolean mBoot;
 
     public Recycler() {
         super(R.layout.fragment_recycler_main);
+        this.mBoot = true;
+        this.mInstructionsList = new LinkedList<>();
     }
 
     @Override
@@ -36,16 +39,60 @@ public class Recycler extends Fragment implements ListAdapter.ClickedTitleListen
         for (int i = 0; i < version.length; i++) {
             mInstructionsList.add(new Instrukcja(titles[version[i]], instructions[version[i]], duration[i]));
         }
-        adapter = new ListAdapter(requireActivity(), mInstructionsList, this);
+        mAdapter = new ListAdapter(requireActivity(), mInstructionsList, this);
         View view = inflater.inflate(R.layout.fragment_recycler_poradnik, container, false);
-        recycler = view.findViewById(R.id.poradniki_rv);
-        recycler.setAdapter(adapter);
-        recycler.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        mRecycler = view.findViewById(R.id.poradniki_rv);
+        mRecycler.setAdapter(mAdapter);
+        mRecycler.setLayoutManager(new LinearLayoutManager(view.getContext(), LinearLayoutManager.VERTICAL, false){
+            @Override
+            public void onLayoutCompleted(RecyclerView.State state) {
+                super.onLayoutCompleted(state);
+                if(mBoot){
+                    scroll(0);
+                    mBoot = false;
+                }
+            }
+        });
         return view;
     }
 
-    @Override
-    public void onTitleClicked(int position) {
+    private void scroll(int position){
+        if(mScrollerInstance!=null){
+            mScrollerInstance.interrupt();
+            position++;
+        }
+        if(position < mInstructionsList.size()) {
+            mScrollerInstance = new Scroller(position);
+            mScrollerInstance.start();
+        }
+    }
 
+    @Override
+    public void onViewClick(int position) {
+        scroll(position-1);
+    }
+
+    private class Scroller extends Thread{
+        private final int position;
+
+        public Scroller(int position){
+            this.position = position;
+        }
+
+        @Override
+        public void run(){
+            requireActivity().runOnUiThread(()->{
+                mRecycler.scrollToPosition(position);
+                mRecycler.scrollBy(0, mRecycler.getHeight()/mAdapter.getItemCount()*3/2);
+                mAdapter.expandAt(position);
+            });
+            try {
+                sleep(mInstructionsList.get(position).getTime());
+                requireActivity().runOnUiThread(() -> scroll(position));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            requireActivity().runOnUiThread(() -> mAdapter.hideAt(position));
+        }
     }
 }
