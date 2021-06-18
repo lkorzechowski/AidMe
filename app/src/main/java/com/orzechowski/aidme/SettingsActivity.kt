419fc3a8d0f12f18836a00e827f47a5cad0144c4
lkorzechowski <lkorzechowski@gmail.com>
2021-06-18 22:13:28 +0200

    0.041 raw policy fragment in settings

--- app/src/main/java/com/orzechowski/aidme/SettingsActivity.kt
+++ app/src/main/java/com/orzechowski/aidme/SettingsActivity.kt
@@ -1,14 +1,45 @@
-package com.orzechowski.aidme;
+package com.orzechowski.aidme
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+import com.orzechowski.aidme.settings.Policy
 
-import android.os.Bundle;
 
-public class SettingsActivity extends AppCompatActivity {
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+        val policyButton = findViewById<Button>(R.id.policy_button)
+        val contributorButton = findViewById<Button>(R.id.contributor_button)
+        val downloadAllTutorialsButton = findViewById<Button>(R.id.download_all_tutorials)
+        val contactButton = findViewById<Button>(R.id.contact_button)
+        val parentLayout = findViewById<ConstraintLayout>(R.id.settings_parent_layout)
+
+        policyButton.setOnClickListener {
+            parentLayout.visibility = View.INVISIBLE
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add<Policy>(R.id.policy_layout)
+            }
+        }
+
+
+        contributorButton.setOnClickListener {
+        }
+
+
+        downloadAllTutorialsButton.setOnClickListener {
+
+        }
+
+        contactButton.setOnClickListener {
+
+        }
     }
 }
---
+++ app/src/main/java/com/orzechowski/aidme/helper/Helper.kt
@@ -0,0 +1,4 @@
+package com.orzechowski.aidme.helper
+
+class Helper {
+}
