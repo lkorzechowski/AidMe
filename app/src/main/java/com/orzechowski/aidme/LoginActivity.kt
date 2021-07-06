9a30ffcac0296fac461a9e1e7657f92738616f76
lkorzechowski <lkorzechowski@gmail.com>
2021-07-06 16:37:09 +0200

    0.058 layouts for login and registration

--- app/src/main/java/com/orzechowski/aidme/LoginActivity.kt
+++ app/src/main/java/com/orzechowski/aidme/LoginActivity.kt
@@ -1,18 +1,57 @@
 package com.orzechowski.aidme
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.commit
 import com.orzechowski.aidme.login.LoginFragment
+import com.orzechowski.aidme.login.RegisterFragment
 
 class LoginActivity : AppCompatActivity() {
-    val mLogin = LoginFragment()
+    private val mLogin = LoginFragment()
+    private val mRegister = RegisterFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         setContentView(R.layout.activity_login)
+        commitLogin()
+        val switchButton = findViewById<Button>(R.id.login_switch_mode_button)
+        val submitButton = findViewById<Button>(R.id.login_submit_button)
+        switchButton.setOnClickListener {
+            val fragmentList: List<*> = supportFragmentManager.fragments
+            var handled: Boolean
+            val t = supportFragmentManager.beginTransaction()
+            for (f in fragmentList) {
+                if(f is LoginFragment){
+                    switchButton.setText(R.string.login_mode_button_login_text)
+                    submitButton.setText(R.string.register_button_text)
+                    handled = f.onBackPressed()
+                    t.remove(mLogin).commit()
+                    if(handled) {
+                        supportFragmentManager.commit {
+                            setReorderingAllowed(true)
+                            add(R.id.login_fragment_bed, mRegister)
+                        }
+                        break
+                    }
+                }
+                else if(f is RegisterFragment) {
+                    switchButton.setText(R.string.login_mode_button_register_text)
+                    submitButton.setText(R.string.login_button_text)
+                    handled = f.onBackPressed()
+                    t.remove(mRegister).commit()
+                    if(handled) {
+                        commitLogin()
+                        break
+                    }
+                }
+            }
+        }
+    }
 
+    private fun commitLogin()
+    {
         supportFragmentManager.commit {
             setReorderingAllowed(true)
             add(R.id.login_fragment_bed, mLogin)
--- app/src/main/java/com/orzechowski/aidme/login/RegisterFragment.java
+++ app/src/main/java/com/orzechowski/aidme/login/RegisterFragment.java
@@ -9,12 +9,12 @@ import androidx.fragment.app.Fragment;
 
 import com.orzechowski.aidme.R;
 
-public class LoginFragment extends Fragment
+public class RegisterFragment extends Fragment
 {
-    @Override
+   @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        return inflater.inflate(R.layout.fragment_register, container, false);
     }
 
     public boolean onBackPressed()
