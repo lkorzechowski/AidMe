009ec073c40b451a36fe6ee46b6ad82d0e1adbef
lkorzechowski <lkorzechowski@gmail.com>
2021-08-05 22:31:51 +0200

    0.137 new idea for how version tree creator ought to be displayed

--- app/src/main/java/com/orzechowski/aidme/creator/VersionTreeComposer.java
+++ app/src/main/java/com/orzechowski/aidme/creator/VersionTreeComposer.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -24,6 +25,8 @@ public class VersionTreeComposer
 {
     private VersionTreeComposerAdapter mLevelOneAdapter, mLevelTwoAdapter, mLevelThreeAdapter,
             mLevelFourAdapter;
+    private VersionTextAdapter mVersionTextAdapter;
+    private final List<Version> mAllVersions;
     private final List<Version> mLevelOneVersions;
     private final List<Version> mLevelTwoVersions = new LinkedList<>();
     private final List<Version> mLevelThreeVersions = new LinkedList<>();
@@ -31,6 +34,7 @@ public class VersionTreeComposer
 
     public VersionTreeComposer(List<Version> versions)
     {
+        mAllVersions = versions;
         mLevelOneVersions = versions;
     }
 
@@ -40,10 +44,13 @@ public class VersionTreeComposer
         FragmentActivity activity = requireActivity();
         View view = inflater
                 .inflate(R.layout.fragment_version_tree_composer, container, false);
+        RecyclerView versionTextRecycler = view.findViewById(R.id.version_text_rv);
         RecyclerView levelOneRecycler = view.findViewById(R.id.version_tree_first_level_rv);
         RecyclerView levelTwoRecycler = view.findViewById(R.id.version_tree_second_level_rv);
         RecyclerView levelThreeRecycler = view.findViewById(R.id.version_tree_third_level_rv);
         RecyclerView levelFourRecycler = view.findViewById(R.id.version_tree_fourth_level_rv);
+        versionTextRecycler.setLayoutManager(new LinearLayoutManager(view.getContext(),
+                LinearLayoutManager.VERTICAL, false));
         levelOneRecycler.setLayoutManager(new LinearLayoutManager(view.getContext(),
                 LinearLayoutManager.HORIZONTAL, false));
         levelTwoRecycler.setLayoutManager(new LinearLayoutManager(view.getContext(),
@@ -52,14 +59,19 @@ public class VersionTreeComposer
                 LinearLayoutManager.HORIZONTAL, false));
         levelFourRecycler.setLayoutManager(new LinearLayoutManager(view.getContext(),
                 LinearLayoutManager.HORIZONTAL, false));
+        mVersionTextAdapter = new VersionTextAdapter(activity);
         mLevelOneAdapter = new VersionTreeComposerAdapter(activity, this, 1);
         mLevelTwoAdapter = new VersionTreeComposerAdapter(activity, this, 2);
         mLevelThreeAdapter = new VersionTreeComposerAdapter(activity, this, 3);
         mLevelFourAdapter = new VersionTreeComposerAdapter(activity, this, 4);
+        versionTextRecycler.setAdapter(mVersionTextAdapter);
         levelOneRecycler.setAdapter(mLevelOneAdapter);
         levelTwoRecycler.setAdapter(mLevelTwoAdapter);
         levelThreeRecycler.setAdapter(mLevelThreeAdapter);
         levelFourRecycler.setAdapter(mLevelFourAdapter);
+        TextView levelTwoLabel = view.findViewById(R.id.version_tree_level_two_label);
+        TextView levelThreeLabel = view.findViewById(R.id.version_tree_level_three_label);
+        TextView levelFourLabel = view.findViewById(R.id.version_tree_level_four_label);
         ItemTouchHelper.SimpleCallback callbackOne = new ItemTouchHelper
                 .SimpleCallback(0, ItemTouchHelper.DOWN | ItemTouchHelper.UP)
         {
@@ -79,6 +91,7 @@ public class VersionTreeComposer
                     mLevelOneVersions.remove(version);
                     mLevelTwoVersions.add(version);
                     mLevelTwoAdapter.setElementList(mLevelTwoVersions);
+                    levelTwoLabel.setVisibility(View.VISIBLE);
                 }
                 mLevelOneAdapter.setElementList(mLevelOneVersions);
             }
@@ -104,6 +117,7 @@ public class VersionTreeComposer
                 if(direction==2) {
                     mLevelThreeVersions.add(version);
                     mLevelThreeAdapter.setElementList(mLevelThreeVersions);
+                    levelThreeLabel.setVisibility(View.VISIBLE);
                 } else if (direction==1) {
                     mLevelOneVersions.add(version);
                     mLevelOneAdapter.setElementList(mLevelOneVersions);
@@ -132,6 +146,7 @@ public class VersionTreeComposer
                 if(direction==2) {
                     mLevelFourVersions.add(version);
                     mLevelFourAdapter.setElementList(mLevelFourVersions);
+                    levelFourLabel.setVisibility(View.VISIBLE);
                 } else if(direction==1) {
                     mLevelTwoVersions.add(version);
                     mLevelTwoAdapter.setElementList(mLevelTwoVersions);
@@ -173,6 +188,7 @@ public class VersionTreeComposer
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState)
     {
         mLevelOneAdapter.setElementList(mLevelOneVersions);
+        mVersionTextAdapter.setElementList(mAllVersions);
     }
 
     @Override
