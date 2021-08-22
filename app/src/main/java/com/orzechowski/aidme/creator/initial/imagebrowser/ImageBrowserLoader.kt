21aff77cfd786bbf19308b997073e1cf55eb53e9
lkorzechowski <lkorzechowski@gmail.com>
2021-08-22 13:30:30 +0200

    0.158 success loading images from memory!!!

--- app/src/main/java/com/orzechowski/aidme/creator/initial/imagebrowser/ImageBrowserLoader.kt
+++ app/src/main/java/com/orzechowski/aidme/creator/initial/imagebrowser/ImageBrowserLoader.kt
@@ -1,76 +1,84 @@
-package com.orzechowski.aidme.creator.initial.imagebrowser;
+package com.orzechowski.aidme.creator.initial.imagebrowser
 
-import android.app.Activity;
-import android.content.ContentUris;
-import android.database.ContentObserver;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.Build;
-import android.os.Bundle;
-import android.provider.MediaStore;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.app.Activity
+import android.content.ContentUris
+import android.database.ContentObserver
+import android.os.Build
+import android.os.Bundle
+import android.provider.MediaStore
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.orzechowski.aidme.R
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.recyclerview.widget.StaggeredGridLayoutManager;
-
-import com.orzechowski.aidme.R;
-
-import java.util.LinkedList;
-import java.util.List;
-
-public class ImageBrowserLoader extends Fragment
+class ImageBrowserLoader : Fragment()
 {
-    private ImageBrowserAdapter mImageBrowserAdapter;
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle bundle)
-    {
-        Activity activity = requireActivity();
-        View view = inflater.inflate(R.layout.fragment_image_browser, container, false);
-        RecyclerView imageRecycler = view.findViewById(R.id.image_recycler_grid);
-        imageRecycler.setLayoutManager(
-                new StaggeredGridLayoutManager(3, RecyclerView.VERTICAL));
-        mImageBrowserAdapter = new ImageBrowserAdapter();
-        imageRecycler.setAdapter(mImageBrowserAdapter);
-        ContentObserver contentObserver = new ContentObserver(null) {
-            @Override
-            public void onChange(boolean selfChange) {
-                loadPhotos();
-            }
-        };
-        activity.getContentResolver().registerContentObserver(
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, true, contentObserver
-        );
-        return view;
+    private var mImageBrowserAdapter = ImageBrowserAdapter()
+    private val mContentObserver: ContentObserver = object : ContentObserver(null) {
+        override fun onChange(selfChange: Boolean) {
+            setAdapterImages()
+        }
     }
 
-    public void loadPhotos()
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, bundle: Bundle?):
+            View
     {
-        Uri uri;
-        if(Build.VERSION.SDK_INT >= 29) uri =
-                MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL);
-        else uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
-        Cursor cursor = requireActivity().getContentResolver().query(
-                uri,
-                new String[]{MediaStore.Images.Media._ID, MediaStore.Images.Media.DISPLAY_NAME,
-                MediaStore.Images.Media.WIDTH, MediaStore.Images.Media.HEIGHT},
-                null,
-                null,
-                "${MediaStore.Images.Media.DISPLAY_NAME} ASC");
-        List<Image> images = new LinkedList<>();
-        while(cursor.moveToNext()) {
-            long id = cursor.getLong(cursor.getColumnIndexOrThrow(MediaStore.Images.Media._ID));
-            images.add(new Image(id,
-                    cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Images.Media
-                            .DISPLAY_NAME)),
-                    cursor.getInt(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.WIDTH)),
-                    cursor.getInt(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.HEIGHT)),
-                    ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)));
+        val activity: Activity = requireActivity()
+        val view = inflater.inflate(R.layout.fragment_image_browser, container, false)
+        val imageRecycler: RecyclerView = view.findViewById(R.id.image_recycler_grid)
+        imageRecycler.layoutManager = StaggeredGridLayoutManager(3, RecyclerView.VERTICAL)
+        imageRecycler.adapter = mImageBrowserAdapter
+        activity.contentResolver.registerContentObserver(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI, true, mContentObserver
+        )
+        setAdapterImages()
+        return view
+    }
+
+    private fun setAdapterImages()
+    {
+        lifecycleScope.launch {
+            mImageBrowserAdapter.setElementList(loadImages())
         }
-        mImageBrowserAdapter.setElementList(images);
-        cursor.close();
+    }
+
+    private suspend fun loadImages(): List<Image>
+    {
+        return withContext(Dispatchers.IO) {
+            val collection = if(Build.VERSION.SDK_INT >= 29) {
+                MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL)
+            } else MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+            val projection = arrayOf(
+                MediaStore.Images.Media._ID, MediaStore.Images.Media.DISPLAY_NAME,
+                MediaStore.Images.Media.WIDTH, MediaStore.Images.Media.HEIGHT)
+            val photos = mutableListOf<Image>()
+            requireActivity().contentResolver.query(collection, projection, null,
+                null, "${MediaStore.Images.Media.DISPLAY_NAME} ASC"
+            )?.use { cursor ->
+                val idColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media._ID)
+                val displayNameColumn =
+                    cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DISPLAY_NAME)
+                val widthColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.WIDTH)
+                val heightColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.HEIGHT)
+
+                while(cursor.moveToNext()) {
+                    val id = cursor.getLong(idColumn)
+                    val displayName = cursor.getString(displayNameColumn)
+                    val width = cursor.getInt(widthColumn)
+                    val height = cursor.getInt(heightColumn)
+                    val contentUri = ContentUris.withAppendedId(
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+                    photos.add(Image(id, displayName, width, height, contentUri))
+                }
+                photos.toList()
+            } ?: listOf()
+        }
     }
 }
