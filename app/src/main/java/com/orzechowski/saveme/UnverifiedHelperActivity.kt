4f58b7f5701bf35f33d5278ab6c97f1ded47161a
lkorzechowski <lkorzechowski@gmail.com>
2021-11-14 15:41:02 +0100

    0.207 verification is fully functional

--- app/src/main/java/com/orzechowski/saveme/UnverifiedHelperActivity.kt
+++ app/src/main/java/com/orzechowski/saveme/UnverifiedHelperActivity.kt
@@ -6,21 +6,16 @@ import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.commit
 import androidx.lifecycle.ViewModelProvider
 import com.android.volley.Request
 import com.android.volley.RequestQueue
-import com.android.volley.toolbox.BasicNetwork
-import com.android.volley.toolbox.DiskBasedCache
-import com.android.volley.toolbox.HurlStack
-import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.*
 import com.orzechowski.saveme.imagebrowser.ImageBrowserLoader
+import com.orzechowski.saveme.volley.StringPost
 import net.gotev.uploadservice.UploadServiceConfig
 import net.gotev.uploadservice.protocols.multipart.MultipartUploadRequest
 import java.util.*
@@ -37,6 +32,7 @@ class UnverifiedHelperActivity : AppCompatActivity(R.layout.activity_unverified_
     private lateinit var mImageBrowser: ImageBrowserLoader
     private lateinit var mQueue: RequestQueue
     private lateinit var viewModelProvider: ViewModelProvider
+    private lateinit var mVerifyButton: Button
     private lateinit var mView: View
     private lateinit var mEmail: String
     private val mUrl = "https://aidme-326515.appspot.com/"
@@ -55,55 +51,74 @@ class UnverifiedHelperActivity : AppCompatActivity(R.layout.activity_unverified_
         mQueue = RequestQueue(cache, network).apply {
             start()
         }
-        val verifyButton = findViewById<Button>(R.id.verify_button)
-        mQueue.add(
-            JsonObjectRequest(Request.Method.GET, mUrl + "documentexists/" + mEmail,
-                null, {
-                    verifyButton.visibility = View.INVISIBLE
+        mVerifyButton = findViewById(R.id.verify_button)
+        mQueue.add(StringRequest(Request.Method.GET, mUrl + "documentexists/" + mEmail,
+            {
+                if(it=="ok") {
                     findViewById<TextView>(R.id.unverified_helper_info).text =
                         resources.getString(R.string.unverified_helper_awaiting_response)
-            },
-            {
-                mView = findViewById(R.id.unverified_view)
-                verifyButton.setOnClickListener {
-                    if(ActivityCompat.checkSelfPermission(this, Manifest.permission
-                            .READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                                ActivityCompat.requestPermissions(this,
-                                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
-                                    121)
-                    } else {
-                            mView.visibility = View.GONE
-                                    mImageBrowser = ImageBrowserLoader(this)
-                                    supportFragmentManager.commit {
-                                add(R.id.fragment_overlay_layout, mImageBrowser)
-                            }
-                    }
+                } else {
+                    Toast.makeText(this, R.string.document_not_found, Toast.LENGTH_SHORT)
+                        .show()
                 }
-            })
-        )
+            }, {
+                Toast.makeText(this, it.message, Toast.LENGTH_SHORT).show()
+            }))
+        mView = findViewById(R.id.unverified_view)
+        mVerifyButton.setOnClickListener {
+            if(ActivityCompat.checkSelfPermission(this, Manifest.permission
+                    .READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this,
+                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 121)
+            } else {
+                mView.visibility = View.GONE
+                mImageBrowser = ImageBrowserLoader(this)
+                supportFragmentManager.commit {
+                    add(R.id.fragment_overlay_layout, mImageBrowser)
+                }
+            }
+        }
     }
 
     override fun imageSubmitted(uri: Uri)
     {
         supportFragmentManager.beginTransaction().remove(mImageBrowser).commit()
-        findViewById<Button>(R.id.verify_button).text =
-            resources.getString(R.string.verify_button_post_upload)
-        val uploadButton = findViewById<Button>(R.id.document_upload_button)
         findViewById<ImageView>(R.id.document_display).setImageURI(uri)
+        mVerifyButton.text = resources.getString(R.string.verify_button_post_upload)
+        val descriptionEdit = findViewById<EditText>(R.id.document_description_input)
+        val descriptionInfo = findViewById<TextView>(R.id.document_description_info)
+        val uploadButton = findViewById<Button>(R.id.document_upload_button)
         uploadButton.visibility = View.VISIBLE
+        descriptionEdit.visibility = View.VISIBLE
+        descriptionInfo.visibility = View.VISIBLE
         uploadButton.setOnClickListener {
             try {
                 thread {
-                    MultipartUploadRequest(
-                        this@UnverifiedHelperActivity,
-                        mUrl + "userdocumentuploadimage/" + mEmail
-                    )
-                        .addFileToUpload(uri.toString(), "file").setMaxRetries(10)
-                        .setUsesFixedLengthStreamingMode(true).setMethod("POST")
-                        .setNotificationConfig { context, uploadId ->
+                    MultipartUploadRequest(this@UnverifiedHelperActivity, mUrl +
+                                "userdocumentuploadimage/" + mEmail).addFileToUpload(uri.toString(),
+                        "file").setMaxRetries(10).setUsesFixedLengthStreamingMode(true)
+                        .setMethod("POST").setNotificationConfig { context, uploadId ->
                             UploadServiceConfig.notificationConfigFactory(context, uploadId)
                         }.startUpload()
                 }
+                val request = StringPost(Request.Method.POST, mUrl +
+                        "userdocumentuploadinfo/" + mEmail, {
+                    uploadButton.visibility = View.INVISIBLE
+                    mVerifyButton.visibility = View.INVISIBLE
+                    descriptionEdit.visibility = View.INVISIBLE
+                    descriptionInfo.visibility = View.INVISIBLE
+                    findViewById<TextView>(R.id.unverified_helper_info).setText(R.string
+                        .document_sent)
+                }, {
+                    val output = if(!it.message.isNullOrEmpty()) it.message!!
+                    else resources.getString(R.string.document_sending_error)
+                    Toast.makeText(this, output, Toast.LENGTH_SHORT).show()
+                }).also {
+                    val text = descriptionEdit.text.toString()
+                    if(text.isNotEmpty()) it.setRequestBody(text)
+                    else it.setRequestBody(" ")
+                }
+                mQueue.add(request)
             } catch(e: Exception) {
                 Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
             }
---
+++ app/src/main/java/com/orzechowski/saveme/volley/StringPost.java
@@ -0,0 +1,36 @@
+package com.orzechowski.saveme.volley;
+
+import androidx.annotation.Nullable;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.nio.charset.StandardCharsets;
+
+public class StringPost extends StringRequest
+{
+    private String mRequestBody;
+
+    public void setRequestBody(String requestBody)
+    {
+        mRequestBody = requestBody;
+    }
+
+    public StringPost(int method, String url, Response.Listener<String> listener,
+                      @Nullable Response.ErrorListener errorListener)
+    {
+        super(method, url, listener, errorListener);
+    }
+
+    @Override
+    public byte[] getBody() {
+        return mRequestBody == null ? null : mRequestBody.getBytes(StandardCharsets.UTF_8);
+    }
+
+
+    @Override
+    public String getBodyContentType()
+    {
+        return "application/json; charset=utf-8";
+    }
+}
