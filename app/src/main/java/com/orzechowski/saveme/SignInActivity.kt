0b77955a278db2d0006efe42bdef3220680083c3
lkorzechowski <lkorzechowski@gmail.com>
2021-11-20 17:29:26 +0100

    0.212 finally debug auth works again, remember - need the debug sha-1 key, then need web, not android key back here. different for when you deploy.

--- app/src/main/java/com/orzechowski/saveme/SignInActivity.kt
+++ app/src/main/java/com/orzechowski/saveme/SignInActivity.kt
@@ -3,8 +3,9 @@ package com.orzechowski.saveme
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
@@ -13,11 +14,12 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.Task
 
-//Bardzo prosta aktywność korzystająca z bibliotek Google w celu zalogowania się na konto użytkownika.
 class SignInActivity : AppCompatActivity()
 {
     private lateinit var mGoogleSignInClient: GoogleSignInClient
-    private val mResultLauncher = registerForActivityResult(StartActivityForResult()) { result ->
+    private val mResultLauncher = registerForActivityResult(ActivityResultContracts
+        .StartActivityForResult()) { result ->
+        Log.w("turkusowy", result.resultCode.toString())
         if(result.resultCode == Activity.RESULT_OK) {
             val data = result.data
             val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
@@ -35,8 +37,7 @@ class SignInActivity : AppCompatActivity()
     {
         super.onCreate(savedInstanceState)
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail()
-            .requestIdToken("1042713587768-2pk21idihc6mbijp2squmr2ekdv65rac.apps" +
-                    ".googleusercontent.com").build()
+            .requestIdToken(getString(R.string.client_id)).build()
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
     }
 
